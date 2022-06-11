a7bb09aa2b871fe48b4167ac641bd0f5bf6375aa
Nick Driscoll <nick.a.mavros@gmail.com>
2022-06-11 02:15:27 -0400

    Minor improvements

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
+extern crate tinyfiledialogs as tfd;
 
 use ash::vk;
 use std::ffi::c_void;
 use std::fs::OpenOptions;
 use std::io::Write;
+use std::path::Path;
 
 fn main() {
     //Initialize the Vulkan API
     let vk_entry = ash::Entry::linked();
     let vk_instance = {
         let app_info = vk::ApplicationInfo {
-            api_version: vk::make_api_version(0, 1, 3, 0),
+            api_version: vk::make_api_version(0, 1, 0, 0),
             ..Default::default()
         };
 
@@ -27,10 +29,12 @@ fn main() {
         unsafe { vk_entry.create_instance(&vk_create_info, None).unwrap() }
     };
 
+    //Write out device info for each physical device found in the system
     unsafe {
         match vk_instance.enumerate_physical_devices() {
             Ok(phys_devices) => {
-                let mut out_file = OpenOptions::new().write(true).create(true).open("./vulkan_physical_device_info.txt").unwrap();
+                let path = "./vulkan_physical_device_info.txt";
+                let mut out_file = OpenOptions::new().write(true).create(true).open(path).unwrap();
                 for device in phys_devices {
                     let mut indexing_features = vk::PhysicalDeviceDescriptorIndexingFeatures::default();
                     let properties = vk_instance.get_physical_device_properties(device);
@@ -43,10 +47,16 @@ fn main() {
                     writeln!(out_file, "{:#?}", properties).unwrap();
                     writeln!(out_file, "{:#?}", physical_device_features).unwrap();
                     writeln!(out_file, "{:#?}", indexing_features).unwrap();
+
+                    let full_path = Path::new(path).canonicalize().unwrap();
+                    let message = format!("Graphics device info written to {}", full_path.to_str().unwrap());
+                    tfd::message_box_ok("Success!", &message, tfd::MessageBoxIcon::Info);
                 }
             }
             Err(e) => {
-                panic!("Unable to enumerate physical devices: {}", e);
+                let message = format!("Unable to enumerate physical devices: {}", e);
+                tfd::message_box_ok("Error enumerating physical devices", &message, tfd::MessageBoxIcon::Error);
+                panic!("{}", message);
             }
         }
     }
